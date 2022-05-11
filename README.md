a1093b8cb14b7364e5a1788bafef7c4a2c465f5c
alufers <alufers@wp.pl>
2022-05-12 01:49:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# mitmproxy2swagger
+
+A tool for automatically converting [mitmproxy](https://mitmproxy.org/) captures to [OpenAPI 3.0](https://swagger.io/specification/) specifications. This means that you can automatically reverse-engineer REST APIs by just running the apps and capturing the traffic.
+
+## Installation
+
+```bash
+$ pip install mitmproxy ruamel.yaml
+```
+
+Then clone the repo and run `mitmproxy2swagger.py` as per examples below.
+
+## Usage
+
+To create a specification by inspecting HTTP traffic you will need to:
+
+1. Capture the traffic by using the mitmproxy tool. I personally recommend using mitmweb, which is a web interface built-in to mitmproxy.
+
+    ```bash
+    $ mitmweb
+    Web server listening at http://127.0.0.1:8081/
+    Proxy server listening at http://*:9999
+    ...
+    ```
+    **IMPORTANT**
+
+    To configure your client to use the proxy exposed by mitm proxy, please consult the [mitmproxy documentation](https://docs.mitmproxy.org/stable/) for more information.
+
+2. Save the traffic to a flow file.
+
+    In mitmweb you can do this by using the "File" menu and selecting "Save":
+    
+    ![](./docs/mitmweb_save.png)
+
+3. Run the first pass of mitmproxy2swagger:
+
+    ```bash
+    $ ./mitmproxy2swagger.py -i <path_to_mitmptoxy_flow> -o <path_to_output_schema> -p <api_prefix>
+    ```
+
+    Please note that you can use an existing schema, in which case the existing schema will be extended with the new data. You can also run it a few times with different flow captures, the captured data will be safely merged.
+
+    `<api_prefix>` is the base url of the API you wish to reverse-engineer. You will need to obtain it by observing the requests being made in mimtproxy.
+
+    For example if an app has made requests like these:
+
+    ```
+    https://api.example.com/v1/login
+    https://api.example.com/v1/users/2
+    https://api.example.com/v1/users/2/profile
+    ```
+
+    The likely prefix is `https://api.example.com/v1`.
+
+4. Running the first pass should have created a section in the schema file like this:
+
+    ```yaml
+    x-path-templates:
+    # Remove the ignore: prefix to generate an endpoint with its URL
+    # Lines that are closer to the top take precedence, the matching is greedy
+    - ignore:/addresses
+    - ignore:/basket
+    - ignore:/basket/add
+    - ignore:/basket/checkouts
+    - ignore:/basket/coupons/attach/{id}
+    - ignore:/basket/coupons/attach/104754
+    ```
+
+    You should edit the schema file with a text editor and remove the `ignore:` prefix from the paths you wish to be generated. You can also adjust the parameters appearing in the paths.
+
+5. Run the second pass of mitmproxy2swagger:
+
+    ```bash
+    $ ./mitmproxy2swagger.py -i <path_to_mitmptoxy_flow> -o <path_to_output_schema> -p <api_prefix> [--examples]
+    ```
+
+    Run the command a second time (with the same schema file). It will pick up the edited lines and generate endpoint descriptions. 
+
+    Please note that mitmproxy2swagger will not overwrite existing endpoint descriptions, if you want to overwrite them, you can delete them before running the second pass.
+
+    Passing `--examples` will add example data to to requests and responses. Take caution when using this option, as it may add sensitive data (tokens, passwords, personal information etc.) to the schema.
+
+## Example output
+
+See the [examples](./examples/). You will find a generated schema there and a html file with the generated documentation (via [redoc-cli](https://www.npmjs.com/package/redoc-cli)).
+
+## License
+MIT
+
---
+++ swagger_util.py
@@ -0,0 +1,136 @@
+import urllib
+
+VERBS = [
+    'add',
+    'create',
+    'delete',
+    'get',
+    'attach',
+    'detach',
+    'update',
+    'push',
+    'extendedcreate',
+    'activate'
+]
+
+# generate a name for the endpoint from the path template
+# POST /api/v1/things/{id}/create -> POST create thing by id
+def path_template_to_endpoint_name(method, path_template):
+    path_template = path_template.strip('/')
+    segments = path_template.split('/')
+    # remove params to a separate array
+    params = []
+    for idx, segment in enumerate(segments):
+        if segment.startswith('{') and segment.endswith('}'):
+            params.append(segment)
+            segments[idx] = '{}'
+    # remove them from the segments
+    segments = [segment for segment in segments if segment != '{}']
+    # reverse the segments
+    segments.reverse()
+    name_parts = []
+    for segment in segments:
+        if segment in VERBS:
+           # prepend to the name_parts
+            name_parts.insert(0, segment.lower())
+        else:
+            name_parts.insert(0, segment.lower())
+            break
+    for param in params:
+        name_parts.append("by " + param.replace('{', '').replace('}', ''))
+        break
+    return method.upper() + ' ' + ' '.join(name_parts)
+
+# when given an url and its path template, generates the parameters section of the request
+def url_to_params(url, path_template):
+    path_template = path_template.strip('/')
+    segments = path_template.split('/')
+    url_segments = url.split('?')[0].strip('/').split('/')
+    params = []
+    for idx, segment in enumerate(segments):
+        if segment.startswith('{') and segment.endswith('}'):
+            params.append({
+                'name': segment.replace('{', '').replace('}', ''),
+                'in': 'path',
+                'required': True,
+                'schema': {
+                    'type': 'number' if url_segments[idx].isdigit() else 'string'
+                }
+            })
+    query_string = urllib.parse.urlparse(url).query
+    if query_string:
+        query_params = urllib.parse.parse_qs(query_string)
+        for key in query_params:
+            params.append({
+                'name': key,
+                'in': 'query',
+                'required': False,
+                'schema': {
+                    'type': 'number' if query_params[key][0].isdigit() else 'string'
+                }
+            })
+    return params
+
+def value_to_schema(value):
+    # check if value is a number
+    if type(value) == int or type(value) == float:
+        return {
+            'type': 'number'
+        }
+    # check if value is a boolean
+    elif type(value) == bool:
+        return {
+            'type': 'boolean'
+        }
+    # check if value is a string
+    elif type(value) == str:
+        return {
+            'type': 'string'
+        }
+    # check if value is a list
+    elif type(value) == list:
+        if len(value) == 0:
+            return {
+                'type': 'array',
+                'items': {}
+            }
+        
+        return {
+            'type': 'array',
+            'items': value_to_schema(value[0])
+        }
+    # check if value is a dict
+    elif type(value) == dict:
+        return {
+            'type': 'object',
+            'properties': {
+                key: value_to_schema(value[key])
+                for key in value
+            }
+        }
+    # if it is none, return null
+    elif value == None:
+        return {
+            'type': 'object'
+        }
+
+MAX_EXAMPLE_ARRAY_ELEMENTS = 10
+MAX_EXAMPLE_OBJECT_PROPERTIES = 150
+# recursively scan an example value and limit the number of elements and properties
+def limit_example_size(example):
+    if type(example) == list:
+        new_list = []
+        for element in example:
+            if len(new_list) >= MAX_EXAMPLE_ARRAY_ELEMENTS:
+                break
+            new_list.append(limit_example_size(element))
+        return new_list
+    elif type(example) == dict:
+        new_dict = {}
+        for key in example:
+            if len(new_dict) >= MAX_EXAMPLE_OBJECT_PROPERTIES:
+                break
+            new_dict[key] = limit_example_size(example[key])
+        return new_dict
+    else:
+        return example
