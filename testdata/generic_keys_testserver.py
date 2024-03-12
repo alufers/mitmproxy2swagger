e22d90c3b54efb0380da5b20cd50424ed2d1b008
Tim Vahlbrock <tim@vahlbrock.de>
2024-03-12 10:23:10 +0100

    reduce duplication in testserver implementations

--- testdata/generic_keys_testserver.py
+++ testdata/generic_keys_testserver.py
@@ -1,57 +1,32 @@
 # -*- coding: utf-8 -*-
-import http.server
 import json
-import socketserver
+
+from testserver import TestServerHandler, launchServerWith
 
 
-class GenericKeysHandler(http.server.BaseHTTPRequestHandler):
-    def do_POST(self):
-        content_length = int(self.headers["Content-Length"])
-        raw_data = self.rfile.read(content_length)
+class GenericKeysHandler(TestServerHandler):
+    def transform_data(self, raw_data):
+        data = json.loads(raw_data)
 
-        try:
-            # Decode received data
+        data["numeric"]["0000"] = {
+            "lorem": "ipsum",
+            "dolor": "sit",
+            "amet": "consectetur",
+        }
+        data["uuid"]["123e4567-e89b-12d3-a456-426614174002"] = {
+            "lorem": "ipsum",
+            "dolor": "sit",
+            "amet": "consectetur",
+        }
+        data["mixed"]["0000"] = {
+            "lorem": "ipsum",
+            "dolor": "sit",
+            "amet": "consectetur",
+        }
 
-            print(raw_data)
-            data = json.loads(raw_data)
-
-            data["numeric"]["0000"] = {
-                "lorem": "ipsum",
-                "dolor": "sit",
-                "amet": "consectetur",
-            }
-            data["uuid"]["123e4567-e89b-12d3-a456-426614174002"] = {
-                "lorem": "ipsum",
-                "dolor": "sit",
-                "amet": "consectetur",
-            }
-            data["mixed"]["0000"] = {
-                "lorem": "ipsum",
-                "dolor": "sit",
-                "amet": "consectetur",
-            }
-
-            # Encode the modified data
-            modified_data = bytes(json.dumps(data), "utf-8")
-
-            # Send the response
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.send_header("Content-length", len(modified_data))
-            self.end_headers()
-            self.wfile.write(modified_data)
-
-        except Exception as e:
-            print(f"Error processing request: {str(e)}")
-            self.send_response(500)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(f"Error processing request: {str(e)}".encode())
+        # Encode the modified data
+        return bytes(json.dumps(data), "utf-8")
 
 
 if __name__ == "__main__":
-    PORT = 8082
-
-    with socketserver.TCPServer(("", PORT), GenericKeysHandler) as httpd:
-        print(f"Serving on port {PORT}")
-        httpd.serve_forever()
+    launchServerWith(GenericKeysHandler)
